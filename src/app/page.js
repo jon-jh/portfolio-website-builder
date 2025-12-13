ace77572673ff670f25eac3b28ab323cdf73146b
jon-jh <hiebert456@gmail.com>
2025-12-13 15:43:08 -0600

    chore: fix navbar overflow and width. center content on page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Home Template (Coming Soon)</h1>
-      <p>This is a placeholder for the Home Page.</p>
-    </main>
-  )
+redirect('/blog')
 }
